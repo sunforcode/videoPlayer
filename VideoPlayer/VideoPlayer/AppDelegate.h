6e75103ae9490988eb75eb4967dfde3beb6dce14
孙永济 <wan@MacBook-Pro-2.local>
2016-10-26 10:09:57 +0800

    创建新项目

---
+++ VideoPlayer/VideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayer
+//
+//  Created by 汤 on 16/10/26.
+//  Copyright © 2016年 com.syj.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayer/VideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoPlayer
+//
+//  Created by 汤 on 16/10/26.
+//  Copyright © 2016年 com.syj.app. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
